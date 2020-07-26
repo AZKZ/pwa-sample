75423c67dd4e90f60e5f07d85150b500606faf16
Kazuya Azegami <k,azegami@time-creators.com>
2020-07-26 09:39:55 +0900

    first commit

---
+++ precache-manifest.2a8faff405cd408d39478dbde0c17151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc8b944d328559a163a",
+    "url": "/pwa-sample/css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa-sample/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "60bb2722684b1dd10bd78eb16dc6f5a6",
+    "url": "/pwa-sample/index.html"
+  },
+  {
+    "revision": "8cc8b944d328559a163a",
+    "url": "/pwa-sample/js/app.fb7771d8.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "/pwa-sample/js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "52c0507b1cc17a713ed5dfe1371b0395",
+    "url": "/pwa-sample/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-sample/robots.txt"
+  }
+]);
